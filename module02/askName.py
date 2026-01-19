f1597c757d0e6331ecd8e8b0bf825e2bf53ef895
Michael <mackutys@gmail.com>
2026-01-19 19:33:04 +0200

    update to simple say Hello and userName

--- module02/askName.py
+++ module02/askName.py
@@ -1,3 +1,3 @@
-print("This program will ask for your name and greet you accordingly :)")
+# This program will ask for your name and greet you accordingly
 userName = input('Enter your name: ')
-print("Nice to meet you, " + userName + "!")
+print("Hello, " + userName + "!")
